36ffadbf4cee8be4d872ffec0f8db76b46ba7b09
Joseph Burkhart <61951318+josephburkhart@users.noreply.github.com>
2025-04-12 13:47:55 -0400

    Update supported browsers

--- collate.py
+++ collate.py
@@ -87,7 +87,8 @@ WEBPAGE_TYPES = {
 FULLY_SUPPORTED_TYPES = ['object-whole', 'link-whole']
 PARTIALLY_SUPPORTED_TYPES = ['object-broken', 'link-broken']
 
-SUPPORTED_BROWSERS = Literal['Firefox', 'Chrome', 'Edge', 'Safari']
+# SUPPORTED_BROWSERS = Literal['Firefox', 'Chrome', 'Edge', 'Safari']
+SUPPORTED_BROWSERS = Literal['Chrome']
 
 WAIT_TIME_SHORT = 0.1
 WAIT_TIME_LONG = 1
